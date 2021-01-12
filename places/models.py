214cdb676f7defe9a9c49b2d65a274407871d99c
pavel <paul601@mail.ru>
2021-01-12 23:39:50 +0500

    Load JSON on site

--- places/models.py
+++ places/models.py
@@ -6,7 +6,7 @@ class Place(models.Model):
     title = models.CharField('Название места', max_length=200)
     place_id = models.CharField('Уникальный идентификатор локации', max_length=100, null=True)
     details_url = models.CharField('Адрес для скачивания доп.сведений о локации', max_length=200, null=True)
-    description_short = models.CharField('Короткое описание', max_length=200)
+    description_short = models.CharField('Короткое описание', max_length=300)
     description_long = models.TextField('Длинное описание')
     lat = models.FloatField('Ширина', max_length=200)
     lon = models.FloatField('Долгота', max_length=200)
--- places/views.py
+++ places/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404
 from .models import Place
 # Create your views here.
+
+
 def show_place_page(request, post_id):
+
     place =get_object_or_404(Place, id=post_id)
-    return HttpResponse(place.title)
+
+    images_url = []
+    for image in place.image_set.all():
+        image_url = image.img.url
+        images_url.append(image_url)
+
+    place_data = {
+        "title": place.title,
+        "imgs":[images_url],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat":place.lat,
+            "lng":place.lon
+        }
+    }
+
+    return JsonResponse(place_data, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
