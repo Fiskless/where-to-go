1e732f3dea4a208d3915dc8948dea5df8e285e19
pavel <paul601@mail.ru>
2021-01-14 20:34:30 +0500

    Add static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/Fiskless/fiskless.pythonanywhere.com/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
