f99e53484282496cfc78ea40ae23b1c71bffbf64
pavel <paul601@mail.ru>
2021-01-14 20:08:54 +0500

    Add venv in wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from dotenv import load_dotenv
+from environs import Env
+env = Env()
+env.read_env()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+project_folder = os.path.expanduser('~/PycharmProjects/Github projects/where-to-go')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
